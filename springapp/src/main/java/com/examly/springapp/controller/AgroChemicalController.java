81660cd012a5a2281463cc46ef868eb3b8722e84
neojarvis <techadmin@iamneo.ai>
2024-06-27 08:23:15 +0000

    Auto-save-commit-27/06/2024--13:53:14

--- springapp/src/main/java/com/examly/springapp/controller/AgroChemicalController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AgroChemicalController.java
@@ -1,8 +1,52 @@
 package com.examly.springapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.examly.springapp.model.AgroChemical;
+import com.examly.springapp.service.AgroChemicalService;
 
 @RestController
 public class AgroChemicalController {
-    
+
+    @Autowired
+    private AgroChemicalService agroChemicalService;
+
+    @PostMapping
+    public ResponseEntity<AgroChemical> createAgroChemical(@RequestBody AgroChemical agroChemical) {
+        AgroChemical createdAgroChemical = agroChemicalService.createAgroChemical(agroChemical);
+        return new ResponseEntity<>(createdAgroChemical,HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AgroChemical> getAgroChemicalById(@PathVariable int id) {
+        AgroChemical agroChemical = agroChemicalService.getAgroChemicalById(id);
+        return ResponseEntity.ok(agroChemical);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AgroChemical>> getAllAgroChemicals() {
+        List<AgroChemical> agroChemicals = agroChemicalService.getAllAgroChemicals();
+        return ResponseEntity.ok(agroChemicals);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AgroChemical> updateAgroChemical(@PathVariable int id, @RequestBody AgroChemical agroChemical) {
+        AgroChemical updatedAgroChemical = agroChemicalService.updateAgroChemical(id, agroChemical);
+        return ResponseEntity.ok(updatedAgroChemical);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAgroChemical(@PathVariable int id) {
+        agroChemicalService.deleteAgroChemical(id);
+        return ResponseEntity.noContent().build();
+    }
 }
