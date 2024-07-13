ad5a6a432649d6b29c98195817d741b285e9d63e
neojarvis <techadmin@iamneo.ai>
2024-07-13 05:13:14 +0000

    Auto-save-commit-13/07/2024--10:43:14

--- helloworld/src/index.js
+++ helloworld/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
+const fname="Nithish";
+const lname="Kumar";
 ReactDOM.render(
   <div>
   <h1>My Favourite Foods</h1>
   <ul>
     <li>Biriyani</li>
+    <li>{fname+" "+lname}</li>
     <li>Chicken</li>
   </ul>
   </div>, document.getElementById('root')
