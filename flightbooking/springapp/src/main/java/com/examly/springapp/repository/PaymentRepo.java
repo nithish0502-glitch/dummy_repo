2d22bf1ed9deca2fbcb10834112bf51554499db5
neojarvis <techadmin@iamneo.ai>
2024-10-17 08:42:17 +0000

    Manual-save-commit-17/10/2024--14:12:16

--- flightbooking/springapp/src/main/java/com/examly/springapp/repository/PaymentRepo.java
+++ flightbooking/springapp/src/main/java/com/examly/springapp/repository/PaymentRepo.java
@@ -3,18 +3,9 @@ package com.examly.springapp.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.examly.springapp.model.Pet;
+import com.examly.springapp.model.Payment;
 
 @Repository
-public interface PaymentRepo extends JpaRepository<Pet, Long> {
-    List<Pet> findByUser_UserId(int userId);
-
-    List<Pet> findByName(String name);
-    
-    List<Pet> findBySpecies(String species);
-    
-    List<Pet> findByBreed(String breed);
-    
-    List<Pet> findByStatus(String status);
+public interface PaymentRepo extends JpaRepository<Payment, Long> {
     
 }
