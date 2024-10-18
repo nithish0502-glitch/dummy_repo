f1da24f8b6bca88ae2b5c734445201e9011f67f9
neojarvis <techadmin@iamneo.ai>
2024-10-18 05:30:10 +0000

    Auto-save-commit-18/10/2024--11:00:10

--- flightbooking/angularapp/src/app/app-routing.module.ts
+++ flightbooking/angularapp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AddFlightComponent } from './components/add-flight/add-flight.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: RegisterComponent}
+  {path: 'signup', component: RegisterComponent},
+  {path: 'add-flight', component: AddFlightComponent}
 ];
 
 @NgModule({
