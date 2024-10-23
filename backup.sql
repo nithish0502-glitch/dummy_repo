d60f544a9eff1ed0f66e66f0fb31d719d364dead
neojarvis <techadmin@iamneo.ai>
2024-10-23 14:34:50 +0000

    Auto-save-commit-23/10/2024--20:04:49

--- backup.sql
+++ backup.sql
@@ -102,7 +102,7 @@ CREATE TABLE `user` (
   `user_role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,7 +111,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin123@gmail.com','1248964327','$2a$10$WkeUb/HFs/Y6xXTHaHYjy.wYu42ECjGfwe6bik5IAJN6RPxSfY6hy','Admin','admin');
+INSERT INTO `user` VALUES (1,'admin123@gmail.com','1248964327','$2a$10$WkeUb/HFs/Y6xXTHaHYjy.wYu42ECjGfwe6bik5IAJN6RPxSfY6hy','Admin','admin'),(2,'user123@gmail.com','4324132211','$2a$10$WfgR4Bg8UnmdZIiswZ3.6e4ad.QlKEJQstRMYYKAVw/RfdzoDe88e','Passenger','user12e');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -124,4 +124,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-23 14:19:50
+-- Dump completed on 2024-10-23 14:34:50
