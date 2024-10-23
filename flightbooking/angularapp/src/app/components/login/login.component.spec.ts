d57d89b344b2bb16809df46baec8a4189e880c16
neojarvis <techadmin@iamneo.ai>
2024-10-23 07:18:05 +0000

    Auto-save-commit-23/10/2024--12:48:03

--- flightbooking/angularapp/src/app/components/login/login.component.spec.ts
+++ flightbooking/angularapp/src/app/components/login/login.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,6 +11,7 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule, HttpClientModule, RouterTestingModule],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
