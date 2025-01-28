31d48c3236578fac6776ac0cb075c375f46b0dd6
neojarvis <techadmin@iamneo.ai>
2025-01-28 06:06:36 +0000

    Auto-save-commit-28/01/2025--11:36:35

---
+++ springapp/src/main/java/com/examly/springapp/model/Project.java
@@ -0,0 +1,60 @@
+package com.examly.springapp.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int projectId;
+    
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Task> tasks;
+
+    // Getters, Setters, Constructors
+    public Project() {}
+    
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ springapp/src/main/java/com/examly/springapp/model/Task.java
@@ -0,0 +1,62 @@
+package com.examly.springapp.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int taskId;
+    
+    private String title;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    @JsonBackReference
+    private Project project;
+
+    // Getters, Setters, Constructors
+    public Task() {}
+    
+    public Task(String title, String status, Project project) {
+        this.title = title;
+        this.status = status;
+        this.project = project;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+}
