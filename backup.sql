0f4a5a3a325fbdd2ee703e375a75d24859516a6a
neojarvis <techadmin@iamneo.ai>
2025-04-23 20:14:17 +0000

    Manual-save-commit-4/24/2025--1:44:16-AM

--- backup.sql
+++ backup.sql
@@ -41,7 +41,7 @@ CREATE TABLE `laptop` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `UKtlki0ww9j5y04x8vunr7022c2` (`user_id`),
   CONSTRAINT `FKooqo9avddwki53oo4fmad0u0q` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -50,7 +50,6 @@ CREATE TABLE `laptop` (
 
 LOCK TABLES `laptop` WRITE;
 /*!40000 ALTER TABLE `laptop` DISABLE KEYS */;
-INSERT INTO `laptop` VALUES (2,1,'Dell','XPS 13','SN123456','Under Maintenance');
 /*!40000 ALTER TABLE `laptop` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,4 +87,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-23 19:59:22
+-- Dump completed on 2025-04-23 20:14:17
