5725a205e525b4eea98c99e7f03d64bfbfcadcc9
neojarvis <techadmin@iamneo.ai>
2025-04-25 11:42:23 +0000

    Auto-save-commit-4/25/2025--5:12:22-PM

--- springapptests/springapp/src/main/java/com/examly/springapp/repository/CustomerRepository.java
+++ springapptests/springapp/src/main/java/com/examly/springapp/repository/CustomerRepository.java
@@ -9,6 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
     List<Customer> findCustomersWithActiveAccounts();
 }
