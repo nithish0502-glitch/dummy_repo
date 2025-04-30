4eb3331d91b9ab5e27ce4897c6929241b60a2efe
neojarvis <techadmin@iamneo.ai>
2025-04-30 11:17:10 +0000

    Auto-save-commit-4/30/2025--4:47:09-PM

--- springapptests/springapp/src/main/java/com/examly/springapp/controller/RestaurantController.java
+++ springapptests/springapp/src/main/java/com/examly/springapp/controller/RestaurantController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import com.examly.springapp.exception.DuplicateRestaurantException;
 import com.examly.springapp.model.Restaurant;
 import com.examly.springapp.service.RestaurantService;
 
@@ -17,22 +19,29 @@ public class RestaurantController {
     private RestaurantService restaurantService;
 
     @PostMapping
-    public ResponseEntity<Restaurant> addRestaurant(@RequestBody Restaurant restaurant) {
+    public ResponseEntity<?> addRestaurant(@RequestBody Restaurant restaurant) {
         try
         {
         Restaurant saved = restaurantService.addRestaurant(restaurant);
         return new ResponseEntity<>(saved, HttpStatus.CREATED);
         }catch(DuplicateRestaurantException e)
         {
-            return new ResponseEntity<>(HttpStatus.CONFLICT).body()
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+        }catch(Exception e)
+        {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Restaurant> getById(@PathVariable Long id) {
+    public ResponseEntity<?> getById(@PathVariable Long id) {
+        try{
         Restaurant restaurant = restaurantService.getRestaurantById(id);
         return ResponseEntity.ok(restaurant);
+        }catch(Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
     }
+}
 
     @GetMapping
     public ResponseEntity<List<Restaurant>> getAll() {
