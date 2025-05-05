d5000d9a194429f5e8bcf54c2d8ce7a065b1451f
neojarvis <techadmin@iamneo.ai>
2025-05-05 08:16:36 +0000

    Manual-save-commit-5/5/2025--1:46:37-PM

--- Fit-Fuel/angularapp/src/app/services/fp.service.spec.ts
+++ Fit-Fuel/angularapp/src/app/services/fp.service.spec.ts
@@ -1,36 +1,3 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { FitnessPlan } from '../model/fp.model';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class FitnessPlannerService {
-  private backendUrl = 'https://ide-fddbbedbb327214235bfdebbddcaecone.premiumproject.examly.io/proxy/3001/'; // Replace with your actual backend URL
-
-  constructor(private http: HttpClient) { }
-
-  getPlans(): Observable<FitnessPlan[]> {
-    return this.http.get<FitnessPlan[]>(this.backendUrl);
-  }
-
-  addPlan(plan: FitnessPlan): Observable<FitnessPlan> {
-    return this.http.post<FitnessPlan>(this.backendUrl, plan);
-  }
-
-  deletePlan(id: number): Observable<void> {
-    const url = `${this.backendUrl}/${id}`;
-    return this.http.delete<void>(url);
-  }
-}
-
-
-
-
-
-
-2
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { FitnessPlannerService } from './fp.service';
