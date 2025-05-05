658a30c737648c7b0fc0f8d097974021eae166b9
neojarvis <techadmin@iamneo.ai>
2025-05-05 07:14:05 +0000

    Auto-save-commit-5/5/2025--12:44:06-PM

--- Fit-Fuel/angularapp/src/app/app.module.ts
+++ Fit-Fuel/angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BikeListComponent } from './bike-list/bike-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BikeListComponent
   ],
   imports: [
     BrowserModule,
