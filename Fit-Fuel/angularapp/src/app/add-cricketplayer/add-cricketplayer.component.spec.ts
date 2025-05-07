337ef9b84e3bceba913c6c91ada4988992c91fd0
neojarvis <techadmin@iamneo.ai>
2025-05-07 11:27:10 +0000

    Auto-save-commit-5/7/2025--4:57:09-PM

--- Fit-Fuel/angularapp/src/app/add-cricketplayer/add-cricketplayer.component.spec.ts
+++ Fit-Fuel/angularapp/src/app/add-cricketplayer/add-cricketplayer.component.spec.ts
@@ -1,25 +1,103 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { AddCricketPlayerComponent } from './add-cricketplayer.component';
+import { CricketPlayerService } from '../services/cricketplayer.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
-import { AddCricketplayerComponent } from './add-cricketplayer.component';
-
-describe('AddCricketplayerComponent', () => {
-  let component: AddCricketplayerComponent;
-  let fixture: ComponentFixture<AddCricketplayerComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddCricketplayerComponent ]
-    })
-    .compileComponents();
-  });
+describe('AddCricketPlayerComponent', () => {
+  let component: AddCricketPlayerComponent;
+  let fixture: ComponentFixture<AddCricketPlayerComponent>;
+  let service: CricketPlayerService;
+  let debugElement: DebugElement;
+  let formBuilder: FormBuilder;
+  let router: Router;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AddCricketplayerComponent);
+    TestBed.configureTestingModule({
+      declarations: [AddCricketPlayerComponent],
+      imports: [ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule],
+      providers: [CricketPlayerService],
+    });
+
+    formBuilder = TestBed.inject(FormBuilder);
+    fixture = TestBed.createComponent(AddCricketPlayerComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(CricketPlayerService);
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigateByUrl').and.returnValue(Promise.resolve(true));
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  fit('should create AddCricketPlayerComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should add a new cricket player when form is valid', () => {
+    const mockPlayer = {
+      name: 'Virat Kohli',
+      age: 35,
+      team: 'India',
+      position: 'Batsman',
+      battingStyle: 'Right-hand bat',
+      bowlingStyle: 'Right-arm medium',
+      totalRuns: 12000,
+      totalWickets: 5,
+      totalMatches: 250
+    };
+
+    spyOn(service, 'addPlayer').and.returnValue(of(mockPlayer));
+    component.playerForm.setValue(mockPlayer);
+
+    component.addNewPlayer();
+
+    expect(component.playerForm.valid).toBeTrue();
+    expect(service.addPlayer).toHaveBeenCalledWith(mockPlayer);
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/players');
+  });
+
+  fit('should require all mandatory form fields', () => {
+    const form = component.playerForm;
+
+    form.setValue({
+      name: '',
+      age: '',
+      team: '',
+      position: '',
+      battingStyle: '',
+      bowlingStyle: '',
+      totalRuns: '',
+      totalWickets: '',
+      totalMatches: ''
+    });
+
+    expect(form.valid).toBeFalse();
+    expect(form.get('name')?.hasError('required')).toBeTrue();
+    expect(form.get('age')?.hasError('required')).toBeTrue();
+    expect(form.get('team')?.hasError('required')).toBeTrue();
+    expect(form.get('position')?.hasError('required')).toBeTrue();
+    expect(form.get('battingStyle')?.hasError('required')).toBeTrue();
+    expect(form.get('bowlingStyle')?.hasError('required')).toBeTrue();
+  });
+
+  fit('should accept valid form even if optional stats are missing', () => {
+    const form = component.playerForm;
+
+    form.setValue({
+      name: 'Jasprit Bumrah',
+      age: 31,
+      team: 'India',
+      position: 'Bowler',
+      battingStyle: 'Right-hand bat',
+      bowlingStyle: 'Right-arm fast',
+      totalRuns: '',
+      totalWickets: '',
+      totalMatches: ''
+    });
+
+    expect(form.valid).toBeTrue();
   });
 });
