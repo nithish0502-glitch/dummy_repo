2143e6448b3e609f01b93b75eb60006c1e9f0186
neojarvis <techadmin@iamneo.ai>
2025-05-12 05:17:07 +0000

    Auto-save-commit-5/12/2025--10:47:05-AM

---
+++ vehiclemicroques/springapparchitect/src/main/java/com/examly/springapparchitect/SpringapparchitectApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springapparchitect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapparchitectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapparchitectApplication.class, args);
+	}
+
+}
